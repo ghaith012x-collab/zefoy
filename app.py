b5c791e8f806fa93bc9b7cd1559db46856f85fdb
ghaith012x-collab <ghaith012x@gmail.com>
2026-07-03 12:14:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -96,4 +96,4 @@ def send():
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
